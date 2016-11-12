566d402304be1e3146e7052c721af078af4516fa
Adam B <adambaranczyk@yahoo.pl>
2016-11-12 17:22:28 +0000

    Customers and vehicles done

--- dealership/customers.py
+++ dealership/customers.py
@@ -1,11 +1,22 @@
 class Person(object):
     def __init__(self, first_name, last_name, email):
-        pass
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.employee = False
+        
+    def is_employee(self):
+        return self.employee
 
 
 class Customer(Person):
-    pass
+    def __init__ (self, first_name, last_name, email):
+        Person.__init__(self, first_name, last_name, email)
+        self.employee = False
+        
 
 
 class Employee(Person):
-    pass
+    def __init__ (self, first_name, last_name, email):
+        Person.__init__(self, first_name, last_name, email)
+        self.employee = True
--- dealership/vehicles.py
+++ dealership/vehicles.py
@@ -1,15 +1,51 @@
 class Vehicle(object):
+    sale_multiplier = 1
+    purchase_multiplier = 1
+    
     def __init__(self, maker, model, year, base_price, miles):
-        pass
+        self.maker = maker
+        self.model = model
+        self.year = year
+        self.base_price = base_price
+        self.miles = miles
+        
+        
+
+    def sale_price(self):
+        return self.base_price * self.sale_multiplier
+        
+
+    def purchase_price(self):
+        return self.sale_price() - (self.purchase_multiplier * self.miles)
 
 
 class Car(Vehicle):
-    pass
-
+   
+    def __init__ (self, maker, model, year, base_price, miles):
+        Vehicle.__init__(self, maker, model, year, base_price, miles)
+        self.sale_multiplier = 1.2
+        self. purchase_multiplier = 0.004
+        self.interest_rate = 1.07
+        
+    
 
 class Motorcycle(Vehicle):
-    pass
+    def __init__ (self, maker, model, year, base_price, miles):
+        Vehicle.__init__(self, maker, model, year, base_price, miles)
+        self.sale_multiplier = 1.1
+        self.purchase_multiplier = 0.009
+        self.interest_rate = 1.03
+        
+  
+        
+        
+    
 
 
 class Truck(Vehicle):
-    pass
+    def __init__ (self, maker, model, year, base_price, miles):
+        Vehicle.__init__(self, maker, model, year, base_price, miles)
+        self.sale_multiplier = 1.6
+        self.purchase_multiplier = 0.02
+        self.interest_rate = 1.11
+
